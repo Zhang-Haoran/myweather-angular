b74e1ca3ae9931baa57ea0e6cd216e62edb46a07
Zhang-Haoran <haoranzhang001@gmail.com>
2021-10-28 12:01:14 +1100

    create current, forecast, searchbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { WeatherComponent } from './weather/weather.component';
-import { CurrentComponent } from './weather/current/current.component';
-import { ForecastComponent } from './weather/forecast/forecast.component';
-import { SearchbarComponent } from './weather/searchbar/searchbar.component';
+import { WeatherComponent } from './components/weather/weather.component';
+import { CurrentComponent } from './components/weather/current/current.component';
+import { ForecastComponent } from './components/weather/forecast/forecast.component';
+import { SearchbarComponent } from './components/weather/searchbar/searchbar.component';
 
 @NgModule({
   declarations: [
