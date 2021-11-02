3a6ba50076c5f24326451ab6bb8fd6d5262937e6
Zhang-Haoran <haoranzhang001@gmail.com>
2021-11-02 14:08:30 +1100

    forecast weather api

--- src/app/components/weather/weather.component.ts
+++ src/app/components/weather/weather.component.ts
@@ -1,34 +1,45 @@
 import { Component, OnInit } from '@angular/core';
-import {OpenWeatherAPIService} from "../../services/open-weather-api.service";
+import { OpenWeatherAPIService } from '../../services/open-weather-api.service';
 
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
-  styleUrls: ['./weather.component.css']
+  styleUrls: ['./weather.component.css'],
 })
 export class WeatherComponent implements OnInit {
-
   currentWeather: any = undefined;
-  searchValue: string = ""
-  constructor(private openWeatherAPIService: OpenWeatherAPIService) { }
+  forecastWeather: any = undefined;
+  searchValue: string = '';
+  constructor(private openWeatherAPIService: OpenWeatherAPIService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   getCurrentWeatherFromAPI(searchValue: string): {} {
-    return this.openWeatherAPIService.getCurrentWeatherFromAPI(searchValue).toPromise().then(currentWeather => {
-      this.currentWeather = currentWeather
-      console.log(this.currentWeather)
-    })
+    return this.openWeatherAPIService
+      .getCurrentWeatherFromAPI(searchValue)
+      .toPromise()
+      .then((currentWeather) => {
+        this.currentWeather = currentWeather;
+        console.log(this.currentWeather);
+      });
   }
 
-  handleSearchValueChange(searchValue: string){
-    this.searchValue = searchValue
-    console.log(this.searchValue)
+  getForecastWeatherFromAPI(searchValue: string): {} {
+    return this.openWeatherAPIService
+      .getForecastWeatherFromAPI(searchValue)
+      .toPromise()
+      .then((forecastWeather) => {
+        this.forecastWeather = forecastWeather;
+        console.log(this.forecastWeather);
+      });
   }
 
-  handleSubmit(event: Event){
-    console.log("hello")
-    this.getCurrentWeatherFromAPI(this.searchValue)
+  handleSearchValueChange(searchValue: string) {
+    this.searchValue = searchValue;
+  }
+
+  handleSubmit(event: Event) {
+    this.getCurrentWeatherFromAPI(this.searchValue);
+    this.getForecastWeatherFromAPI(this.searchValue);
   }
 }
--- src/app/services/open-weather-api.service.ts
+++ src/app/services/open-weather-api.service.ts
@@ -8,8 +8,14 @@ import {Observable} from "rxjs";
 })
 export class OpenWeatherAPIService {
   constructor(private http: HttpClient) { }
+
   getCurrentWeatherFromAPI(searchValue: string): Observable<any>{
     const currentWeatherURL = `${environment.WEATHER_API_URL}weather?q=${searchValue}&APPID=${environment.APP_KEY}&units=metric`;
     return this.http.get<any>(currentWeatherURL);
+  }
+
+  getForecastWeatherFromAPI(searchValue: string): Observable<any>{
+    const forecastWeatherURL = `${environment.WEATHER_API_URL}forecast?q=${searchValue}&APPID=${environment.APP_KEY}&units=metric`;
+    return this.http.get<any>(forecastWeatherURL);
   }
 }
