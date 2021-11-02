bb395f3d167d9e7b5f729b29f1dfa68960d6b643
Zhang-Haoran <haoranzhang001@gmail.com>
2021-11-02 16:58:34 +1100

    publish

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  WEATHER_API_URL: "https://api.openweathermap.org/data/2.5/",
+  APP_KEY: "2e9fc10d5be83f64466b524a0669c79d"
 };
